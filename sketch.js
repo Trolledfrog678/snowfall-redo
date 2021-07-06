0b1547fbf5c64c9da51e11321870641775fae2ef
Trolledfrog678 <76543612+Trolledfrog678@users.noreply.github.com>
2021-07-06 16:22:14 -0700

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,13 @@
+var Snow,SnowBackground
+
+
+function setup() {
+  createCanvas(800,400);
+  Snow=createSprite(255, 255, 50, 50);
+}
+
+function draw() {
+  background(255,255,255);  
+  drawSprites();
+  snow.display();
+}
---
+++ snow.js
@@ -0,0 +1,15 @@
+class snow {
+
+    constructor() {
+     
+    }
+  
+    hide(){
+     
+    }
+  
+    display(){
+        Snow=createSprite(255, 255, 50, 50);
+    }
+}
+  
